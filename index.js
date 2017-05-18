81a74b67cbaa9ecfa513f41a5b65df3d3e2ec974
Frase <mrfrase3@gmail.com>
2017-05-18 18:25:24 +0800

    cleaned up https logic so server starts when ready

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var io;
 //Initiate the webserver with http/https
 var server;
 var redirectServer;
+var port;
 if(config.server.https.enabled){
     var keys = {
         key: fs.readFileSync(config.server.https.key || "certs/key.pem", "utf8"), 
@@ -21,21 +22,19 @@ if(config.server.https.enabled){
     if(config.server.http.enabled){
         redirectServer = require('http').Server();
         redirectServer.on('request', function(req, res){
-            let port = ':' + (config.server.https.port || 3001);
-            if(port == ':443') port = '';
+            let report = ':' + (config.server.https.port || 3001);
+            if(report == ':443') report = '';
             res.writeHead(301,
-              {Location: 'https://'+req.headers.host+port+req.url}
+              {Location: 'https://'+req.headers.host+report+req.url}
             );
             res.end();
         });
         redirectServer.listen((config.server.http.port || 3000));
     }
-    server.listen((config.server.https.port || 3001));
-    console.log("Server started on port: " + (config.server.https.port || 3001));
+    port = (config.server.https.port || 3001);
 } else {
     server = require('http').Server(app);
-    server.listen((config.server.http.port || 3000));
-    console.log("Server started on port: " + (config.server.http.port || 3000));
+    port = (config.server.http.port || 3000);
 }
 io = require('socket.io')(server);
 
@@ -49,5 +48,8 @@ app.use("/common", express.static( __dirname + "/common" ));
 
 app.get('/', function (req, res) {
     var fill = {isPageIndex: true};
-	res.render('index');
+	res.render('index', fill);
 });
+
+server.listen(port)
+console.log("Server started on port: " + port);
